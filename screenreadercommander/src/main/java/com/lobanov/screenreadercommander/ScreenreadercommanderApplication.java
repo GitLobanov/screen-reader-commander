41dcf49445fd46398f1286bdc45df29c2fe198aa
GitLobanov <lobanovgithub@gmail.com>
2024-09-20 09:10:03 +0700

    Init project

---
+++ screenreadercommander/src/main/java/com/lobanov/screenreadercommander/ScreenreadercommanderApplication.java
@@ -0,0 +1,13 @@
+package com.lobanov.screenreadercommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenreadercommanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenreadercommanderApplication.class, args);
+    }
+
+}
